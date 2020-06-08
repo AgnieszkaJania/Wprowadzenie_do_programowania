6598b06f8dd695b122ff9a3780370fd94ca6d773
Agnieszka <agnieszka.jania1995@gmail.com>
2020-06-08 20:36:48 +0200

    Pierwsze zmiany

---
+++ Model_Gry/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Model_Gry
+{   
+    public class ModelGry
+    {
+        public int liczbaWylosowana;
+        public int ileCyfr = 4;
+        public int[] cyfry;
+        public bool flaga = true;
+        
+        public ModelGry(int min, int max)
+        {   
+            
+            if (min < 0 || max < 0)
+                Console.WriteLine("Proszę podać liczby nieujemne");
+
+
+            if (min > max)
+                throw new ArgumentException("zły przedział do losowania");
+            
+            cyfry = new int[ileCyfr];
+            for (int i = 0; i < ileCyfr; i++)
+            {
+                liczbaWylosowana = (new Random()).Next(min, max + 1);
+                cyfry[i] = liczbaWylosowana;
+
+            }
+            for(int j = 0; j < cyfry.Length; j++)
+            {
+                Console.Write($"{cyfry[j]} ");
+            }
+            Console.WriteLine();
+            
+
+            
+        }
+        public void Przerwa()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Write(' ');
+                Thread.Sleep(100);
+
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+        }
+
+
+
+        public Odpowiedz Propozycja(int[] propzycja)
+        {
+            Odpowiedz odp;
+            if (cyfry.Length != propzycja.Length)
+                throw new ArgumentException("Podano błędne cyfry");
+            for(int i = 0; i < cyfry.Length; i++)
+            {
+                if(cyfry[i] == propzycja[i])
+                {
+                    flaga = true;
+                }
+                else
+                {
+                    flaga = false;
+                    break;
+                }
+            }
+            if (flaga)
+            {
+                odp = Odpowiedz.dobrze;
+            }
+            else
+            {
+                odp = Odpowiedz.źle;
+            }
+            return odp;
+
+
+
+        }
+        //public void wynikGry(Odpowiedz odp)
+        //{
+        //    if(odp == -1)
+        //    {
+
+        //    }
+        //}
+        public enum StanGry
+        {
+            wTrakcie,
+            przegrana,
+
+
+
+        }
+
+        public enum Odpowiedz
+        {
+            źle = -1,
+            dobrze = 1
+        }
+
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Projekt_Wprowadzenie_do_programowania_Agnieszka_Jania/Program.cs
+++ Projekt_Wprowadzenie_do_programowania_Agnieszka_Jania/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model_Gry;
+using System;
+using System.Threading;
 
 namespace Projekt_Wprowadzenie_do_programowania_Agnieszka_Jania
 {
@@ -6,8 +8,33 @@ namespace Projekt_Wprowadzenie_do_programowania_Agnieszka_Jania
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("start");
-            Console.WriteLine("hello");
+            Console.WriteLine("Witaj w grze - zapamiętaj liczby");
+            Console.Write("Podaj przedział losowania oddzielony spacją ");
+            string[] dane = Console.ReadLine().Split(' ');
+            int[] przedzial = Array.ConvertAll(dane, int.Parse);
+            int powtorka = 3;
+            for(int i = 0; i < powtorka; i++)
+            {
+                Console.WriteLine("Zapamiętaj w czasie wylosowane poniżej liczby, a następnie przepisz je");
+                ModelGry gra = new ModelGry(przedzial[0], przedzial[1]);
+
+                gra.Przerwa();
+                Console.WriteLine("Przepisz liczby w kolejności");
+                string[] odpowiedz = Console.ReadLine().Split(' ');
+                int[] odp = Array.ConvertAll(odpowiedz, int.Parse);
+                var Odp = gra.Propozycja(odp);
+                Console.WriteLine(Odp);
+            }
+
+
+            
+           
+            
+            
+
+
+
+
         }
     }
 }
